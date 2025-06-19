4b45ede0e3f14cc25dc7b94c558fcc5f922751aa
Kleber Coelho <klebercoelhogt@gmail.com>
2025-06-19 20:34:46 -0300

    #

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from "express";
-
+import cors from "cors";
 import routes from "./routes/routes";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello from app!");
 });
