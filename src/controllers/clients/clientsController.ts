89fad33d7af931bc6e01b9bd09887020bc96337c
Kleber Coelho <klebercoelhogt@gmail.com>
2025-07-22 21:13:17 -0300

    criado controller de contas

--- src/controllers/clients/clientsController.ts
+++ src/controllers/clients/clientsController.ts
@@ -55,6 +55,62 @@ async function createClient(req: Request, res: Response) {
   }
 }
 
+async function getAccountsByClient(req: Request, res: Response) {
+  const { id } = req.params;
+
+  try {
+    if (!id) {
+      res.status(400).json({
+        error: {
+          message: "Preencha o id do cliente antes de buscar!",
+          status: 400,
+          success: false,
+        },
+      });
+      return;
+    }
+
+    const userExists = await prisma.client.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!userExists) {
+      res.status(404).json({
+        error: {
+          message: "Cliente não encontrado!",
+          status: 404,
+          success: false,
+        },
+      });
+      return;
+    }
+    const getUser = await prisma.client.findUnique({
+      where: {
+        id: Number(id),
+      },
+
+      include: { accounts: true },
+    });
+    res.json({
+      message: "Cliente encontrado com sucesso!",
+      status: 200,
+      success: true,
+      cliente: getUser,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message:
+        "Erro ao buscar o cliente com a conta! Provavelmente um erro no servidor!",
+      status: 500,
+      success: false,
+    });
+  }
+}
+
 export default {
   createClient,
+  getAccountsByClient,
 };
--- src/routes/clients/clientsRoute.ts
+++ src/routes/clients/clientsRoute.ts
@@ -4,5 +4,6 @@ import clientsController from "../../controllers/clients/clientsController";
 const clientRouter = Router();
 
 clientRouter.post("/create", clientsController.createClient);
+clientRouter.get("/get/:id", clientsController.getAccountsByClient);
 
 export default clientRouter;
